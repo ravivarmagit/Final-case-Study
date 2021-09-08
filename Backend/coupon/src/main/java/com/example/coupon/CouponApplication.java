5d686dbbefb8d6102020d37156a3330f8fde3904
ravivarmagit <86913018+ravivarmagit@users.noreply.github.com>
2021-09-08 19:18:18 +0530

    Add files via upload

---
+++ Backend/coupon/src/main/java/com/example/coupon/CouponApplication.java
@@ -0,0 +1,24 @@
+package com.example.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableEurekaClient
+public class CouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApplication.class, args);
+	}
+	
+
+
+
+}
